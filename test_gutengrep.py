fe24d0b4870a6a2f118795893c3ab35d72e72438
hugovk <hugovk@users.noreply.github.com>
2017-09-30 00:10:56 +0300

    unittest2 not needed since Python 2.6 dropped

--- test_gutengrep.py
+++ test_gutengrep.py
@@ -4,10 +4,7 @@
 Unit tests for gutengrep.py
 """
 from __future__ import print_function, unicode_literals
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+import unittest
 
 import gutengrep
 
